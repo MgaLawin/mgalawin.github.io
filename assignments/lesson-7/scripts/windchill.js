2536131e188ff88a3dc22998f48fda55c8553ca6
MgaLawin <35164043+MgaLawin@users.noreply.github.com>
2018-02-24 17:07:11 -0800

    W07-10

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,16 +1,11 @@
-function doInputOutput() {
-  var tempF = parseFloat(document.getElementById('Temperature').value);
-  var speed = parseFloat(document.getElementById('WindSpeed').value);
-  var windChillTemp = 0;
-  if (tempF <= 50 && speed > 3) {
-    var windChillTemp = windChill(tempF, speed);
-    document.getElementById('outputDiv').innerHTML = windChillTemp + "°F";
-  } else {
-    document.getElementById('outputDiv').innerHTML = tempF + "°F";
-  }
-}
-
-function windChill(tempF, speed) {
+var tempH = parseFloat(document.getElementById('hightemp').value);
+var tempL = parseFloat(document.getElementById('lowtemp').value);
+var tempF = (tempH + tempL) / 2;
+var speed = parseFloat(document.getElementById('windspeed').value);
+var windChillTemp = 0;
+if (tempF <= 50 && speed > 3) {
   var windChillTemp = 35.74 + (0.6215 * tempF) - (35.75 * speed ** .16) + (0.4275 * tempF * speed ** .16);
-  return windChillTemp;
+  document.getElementById("windChillTempValue").innerHTML = windChillTemp;
+} else {
+  document.getElementById('windChillTempValue').innerHTML = tempF;
 }
