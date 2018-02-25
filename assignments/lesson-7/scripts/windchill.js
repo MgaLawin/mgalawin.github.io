6f5d919bed7ee0f5e2f6639a0f66a678aa0c8946
MgaLawin <35164043+MgaLawin@users.noreply.github.com>
2018-02-24 17:49:03 -0800

    W07-12

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,10 +1,13 @@
-var tempH = parseFloat(document.getElementById('hightemp').value);
-var tempL = parseFloat(document.getElementById('lowtemp').value);
+var tempH = parseFloat(document.getElementById('hightemp').innerHTML);
+var tempL = parseFloat(document.getElementById('lowtemp').innerHTML);
 var tempF = (tempH + tempL) / 2;
-var speed = parseFloat(document.getElementById('windspeed').value);
+var speed = parseFloat(document.getElementById('windspeed').innerHTML);
 var windChillTemp = 0;
 if (tempF <= 50 && speed > 3) {
   var windChillTemp = 35.74 + (0.6215 * tempF) - (35.75 * speed ** .16) + (0.4275 * tempF * speed ** .16);
+  var digits = 1;
+  var multiplier = Math.pow(10, digits);
+  windChillTemp = Math.round(windChillTemp * multiplier) / multiplier;
   document.getElementById("windChillTempValue").innerHTML = windChillTemp;
 } else {
   document.getElementById('windChillTempValue').innerHTML = tempF;
